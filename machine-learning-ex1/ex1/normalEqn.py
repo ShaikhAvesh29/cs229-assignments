5a13b67afa792bd401e2084a5f0db414d7c927e2
Avesh Shakil Shaikh <aveshshaikh2937@gmail.com>
2026-09-14 09:31:34 +0530

    Implement closed form solution for linear regression

--- machine-learning-ex1/ex1/normalEqn.py
+++ machine-learning-ex1/ex1/normalEqn.py
@@ -7,6 +7,7 @@ def normal_eqn(X, y):
     # Instructions : Complete the code to compute the closed form solution
     #                to linear regression and put the result in theta
     #
+    theta = np.linalg.pinv(X.T @ X) @ X.T @ y
 
 
     return theta
